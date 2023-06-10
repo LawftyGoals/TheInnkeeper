4f71dd1d1ee8f98bb66f49ef79376472783c36e4
Lawfty Goals <mymsnadressis@hotmail.com>
2023-06-11 00:29:57 +0200

    working with birthrates

--- Scripts/WorldSystemScripts/PopulationMechanics.cs
+++ Scripts/WorldSystemScripts/PopulationMechanics.cs
@@ -3,6 +3,7 @@ using System;
 
 public partial class PopulationMechanics
 {
+    private int _totalPopulation;
     private int _numberOfFemales;
 
     //base the number of fertile females on plausible number of females between age 12-48, at this point it is a 60% of total female pop.
@@ -10,7 +11,9 @@ public partial class PopulationMechanics
     private int _numberOfMales;
     private float _percentageFertileM;
 
-    private float _birthRate;
+    private float _birthFactor;
+
+    private float _birthProgress;
 
     public int PopulationTotal
     {
@@ -22,19 +25,36 @@ public partial class PopulationMechanics
         int males = 140,
         float femalePer = 0.60f,
         float malePer = 0.95f,
-        float birthRate = 0.10f
+        float birthFactor = 0.04f
     )
     {
         _numberOfFemales = females;
         _numberOfMales = males;
         _percentageFertileF = femalePer;
         _percentageFertileM = malePer;
-        _birthRate = birthRate;
+        _birthFactor = birthFactor; // 4% is pretty close to norway
+        _birthProgress = (float)new Random().NextDouble();
     }
 
     public void births()
     {
-        GD.Print(_numberOfFemales * _percentageFertileF * _percentageFertileM * _birthRate / 365);
+        if (_birthProgress >= 1)
+        {
+            double mOrF = new Random().NextDouble();
+            if (mOrF < 0.5145631067961165)
+            {
+                _numberOfMales++;
+            }
+            else
+            {
+                _numberOfFemales++;
+            }
+            _birthProgress = _birthProgress % 1;
+        }
+
+        GD.Print(
+            _numberOfFemales * _percentageFertileF * _percentageFertileM * _birthFactor / 365 / 24
+        );
     }
 
     public void deaths() { }
