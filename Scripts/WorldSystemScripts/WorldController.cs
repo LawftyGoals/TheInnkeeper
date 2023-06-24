935d2ecedcbb76c7ea99a74325762c4343978e3a
Lawfty Goals <mymsnadressis@hotmail.com>
2023-06-25 00:52:21 +0200

    made inn, made edits to accomodate

--- Scripts/WorldSystemScripts/WorldController.cs
+++ Scripts/WorldSystemScripts/WorldController.cs
@@ -3,10 +3,16 @@ using System;
 
 public partial class WorldController : Node
 {
+    private InnController _playerInn;
+    private Character _playerCharacter;
+
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
     {
         SetProcess(false);
+        _playerInn = GetNode<InnController>("NodeInnController");
+        _playerCharacter = GetNode<Character>("NodePlayerCharacter");
+        _playerInn.InnOwner = _playerCharacter;
     }
 
     // Called every frame. 'delta' is the elapsed time since the previous frame.
